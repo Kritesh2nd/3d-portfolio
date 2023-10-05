40cfa51b56132a609d9616cf7b1e557e55aa4d95
kritesh2nd <kritesh2nd@gmail.com>
2023-10-05 20:46:42 +0545

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Kritesh</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden"/> interfaces and web application.
+            I develop 3D visuals, user <br className="sm:block hidden"/> interfaces and web application
           </p>
         </div>
       </div>
